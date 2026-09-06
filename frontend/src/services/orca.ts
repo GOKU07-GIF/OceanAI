7aca3414fb00bc9c88cea173cffd55b07862bf07
GOKU07-GIF <sacgupta07@gmail.com>
2026-09-06 05:45:51 -0700

    Increase ORCA frontend request timeout

--- frontend/src/services/orca.ts
+++ frontend/src/services/orca.ts
@@ -1,7 +1,11 @@
 import api from "./api";
 import type { OrcaRequest, OrcaResponse } from "../types/orca";
 
+const ORCA_REQUEST_TIMEOUT_MS = 120_000;
+
 export async function askOrca(data: OrcaRequest): Promise<OrcaResponse> {
-  const response = await api.post<OrcaResponse>("/orca/plan", data);
+  const response = await api.post<OrcaResponse>("/orca/plan", data, {
+    timeout: ORCA_REQUEST_TIMEOUT_MS,
+  });
   return response.data;
 }
